f5d571740b009647634434e1ab85390888c64580
Alexander Staemmler <Alexander.Staemmler@Student.HTW-Berlin.de>
2023-05-24 13:02:12 +0200

    Added example for promise as comment in userController.

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,4 +33,14 @@ exports.saveUser = async (req, res) => {
     } catch (error) {
       res.send(error);
     }
+
+    /* // Das hier sollte bereits als promise funktionieren anstatt try catch von oben
+    newSubscriber.save()
+    .then((result) => {
+      res.render("thanks");
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+    */
 };
