a6f5ad8cda844de8868c7448890f59634deeae86
Alexander Staemmler <Alexander.Staemmler@Student.HTW-Berlin.de>
2023-05-24 12:46:45 +0200

    Added getSub...Page and saveUser in user controller.

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,27 @@ exports.getAllUsers = (req, res, next) => {
    next(error);
  });
 };
+
+// Überlegen welche seite sich da am bsten anbietet, zur zeit contacts aus dem buch aber eine dedizierte registrierungs page würde sinn machen.
+exports.getSubscriptionPage = (req, res) => {
+    res.render("contact");
+};
+  
+exports.saveUser = async (req, res) => {
+    let newUser = new User({
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      moderatorStatus: req.body.moderatorStatus, // hier nimmt er noch die angabe aus dem req bdy, das macht aber keinen sinn. überlegen wie man das abändert.
+      password: req.body.password,
+      zipCode: req.body.zipCode //Bin mir nicht sicher ob der hier benötigt wird.
+    });
+  
+    try {
+      await newUser.save();
+      res.render("thanks");
+    } catch (error) {
+      res.send(error);
+    }
+};
