1632a25f1baab1acebd16a0f8f62f9af3384794d
Alexander Staemmler <Alexander.Staemmler@Student.HTW-Berlin.de>
2023-06-29 12:05:18 +0200

    Changes ins storyController.

--- routes/storyRoute.js
+++ routes/storyRoute.js
@@ -4,10 +4,10 @@ const storyController = require("../controllers/storyController");
 router
   .get("/uploadStory", storyController.getStoryUploadForm)
   .post("/uploadStory", storyController.saveStory)
-  .get("/stories", storyController.getAllStorys, (req, res, next) => {
+  .delete("/id:/delete", storyController.deleteStory, storyController.redirectView)
+  .get("/id:", storyController.showStory)
+  .get("/", storyController.getAllStorys, (req, res, next) => {
     console.log(req.data);
     res.send(req.data);
-  })
-  .delete("/id:/delete", storyController.deleteStory, storyController.redirectView)
-  .get("/id:", storyController.showStory);
+  });
 module.exports = router;
